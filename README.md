f8702c7741a49b4185887477d08bb34ba67c0edb
Pheonnexx <pheonn@hotmail.co.uk>
2016-06-24 14:56:04 +0100

    Update to requirements, readme, added example character

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# character_sheets_api
+# Character Sheets Api
+
+Written in Python 3.
+
+Create a virtualenv.
+
+Pip install the requirements.
+
+Run this command:
+
+<code> python3 run.py </code>
--- app/views.py
+++ app/views.py
@@ -1,7 +1,28 @@
+from flask import jsonify
 from app import app
+
 
 @app.route('/')
 @app.route('/hello')
 def index():
-    message = "Hello World"
+    message = "Hello"
     return message
+
+
+@app.route('/character/<name>')
+def character(name):
+    return_character = _get_character(name)
+    return jsonify(return_character)
+
+
+def _get_character(name):
+    return_character = {
+        "name": name,
+        "character_name": "Erica",
+        "game_system": "Dungeons and Dragons 5th",
+        "character_details": {
+            "level": 5,
+            "race": "dwarf"
+        }
+    }
+    return return_character
